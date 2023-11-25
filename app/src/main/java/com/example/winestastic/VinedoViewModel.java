7f32c6da2ec01cb2e0de75e8381f2c8b8145afd8
paolafg <l19141143@queretaro.tecnm.mx>
2023-11-25 00:44:05 -0600

    pantalla de informacion de vinedos

---
+++ app/src/main/java/com/example/winestastic/VinedoViewModel.java
@@ -0,0 +1,7 @@
+package com.example.winestastic;
+
+import androidx.lifecycle.ViewModel;
+
+public class VinedoViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/winestastic/vinedo.java
@@ -0,0 +1,36 @@
+package com.example.winestastic;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class vinedo extends Fragment {
+
+    private VinedoViewModel mViewModel;
+
+    public static vinedo newInstance() {
+        return new vinedo();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_vinedo, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(VinedoViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
